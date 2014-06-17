8a31d0cab2fa64c25c4a60b1e57756ca5becac16
Corinne McElwain <cori_mcelwain@intuit.com>
2014-06-17 16:25:07 -0700

    added login

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -36,34 +36,38 @@ angular.module('spearmintWebApp', [
         templateUrl: 'partials/signup',
         controller: 'SignupCtrl'
       })
+      .when('/login', {
+        templateUrl: 'partials/login',
+        controller: 'LoginCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
       
     $locationProvider.html5Mode(true);
       
-    // Intercept 401s and redirect you to login
-  //   $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
-  //     return {
-  //       'responseError': function(response) {
-  //         if(response.status === 401) {
-  //           $location.path('/login');
-  //           return $q.reject(response);
-  //         }
-  //         else {
-  //           return $q.reject(response);
-  //         }
-  //       }
-  //     };
-  //   }]);
-  // })
+    //Intercept 401s and redirect you to login
+    $httpProvider.interceptors.push(['$q', '$location', function($q, $location) {
+      return {
+        'responseError': function(response) {
+          if(response.status === 401) {
+            $location.path('/login');
+            return $q.reject(response);
+          }
+          else {
+            return $q.reject(response);
+          }
+        }
+      };
+    }]);
+  });
   // .run(function ($rootScope, $location, Auth) {
 
   //   // Redirect to login if route requires auth and you're not logged in
   //   $rootScope.$on('$routeChangeStart', function (event, next) {
       
-  //     if (next.authenticate && !Auth.isLoggedIn()) {
+  //     if (next.authenticate) {
   //       $location.path('/login');
   //     }
   //   });
-  });
+  // });
