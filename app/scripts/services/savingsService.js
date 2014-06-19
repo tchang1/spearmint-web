90ed547bf119b11b3f5fa67e0db6e6c5b019dfb2
Tony Chang <ampedlemon@gmail.com>
2014-06-19 15:18:47 -0700

    savings service

--- app/scripts/services/savingsService.js
+++ app/scripts/services/savingsService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('spearmintWebApp').factory('savingsService', ['RESTService', '$q', 'logger', 'config',
-    function(RESTService, userService, $q, logger, config) {
+    function(RESTService, $q, logger, config) {
         return {
             getSavings: function() {
                 var deferred = $q.defer();
@@ -25,25 +25,26 @@ angular.module('spearmintWebApp').factory('savingsService', ['RESTService', '$q'
             createNewSavings: function(savings) {
                 var deferred = $q.defer();
 
-                if (!goal) {
+                if (!savings) {
                     deferred.reject('Cannot save a null savings');
                     return deferred.promise;
                 }
 
                 savings.goalid = (savings.goalid) ? savings.goalid : '';
-                savings.savingsAmount = (goal.amountSaved) ? savings.savingsAmount : 0;
+                savings.savingsAmount = (savings.savingsAmount) ? savings.savingsAmount : 0;
+
+                logger.log('Posting savings');
 
 
-
-                    RESTService.post({url: config.server.baseURL + config.server.savingsURL,
-                        data: goal}).then(
+                RESTService.post({url: config.server.baseURL + config.server.savingsURL,
+                        data: savings}).then(
                         // success handler
                         function(data) {
-                            logger.log('Goal created successfully.');
+                            logger.log('Savings created successfully.');
                             deferred.resolve(data);
                         },
                         function(error) {
-                            logger.log('An error occurred while creating goal');
+                            logger.log('An error occurred while creating savings');
                             logger.log(error);
                             deferred.reject(error);
                         });
