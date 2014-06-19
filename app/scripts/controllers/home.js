9000f8eb547f3dc84ee2beb6c6e22934bfbe0a36
Corinne McElwain <cori_mcelwain@intuit.com>
2014-06-19 11:09:20 -0700

    fixed image transition and cleaned up code a bit

--- app/scripts/controllers/home.js
+++ app/scripts/controllers/home.js
@@ -3,9 +3,9 @@
 angular.module('spearmintWebApp')
   .controller('HomeCtrl', ['$scope', '$location', 'logger', 'progressIndicator', 'goal', 'goalService', function ($scope, $location, logger, progressIndicator, goal, goalService) {
 
-     // To Do: delete this code once service is created 
+    // To Do: delete this code once service to get correct image is created 
     // -------------------------------
-    var currentImage = 0; 
+    var imageNum = 0; 
 
     var goalFolders = ["pet", "travel"];
 
@@ -25,35 +25,46 @@ angular.module('spearmintWebApp')
     };
     // -------------------------------
 
-    $scope.message = "Push and hold to Save";
-    progressIndicator.initWithCanvas(document.getElementById('progressIndicator'));
+ 
+    function PreloadImage (src) {
+        var img = new Image ();
+        img.src = src;
+        logger.log(src + " loaded");
+        return img; 
+    }
 
+    // SETUP Page with initial message, progress indicator, and load the images to show 
+    $scope.message = "Push and hold to Save";
+
+    progressIndicator.initWithCanvas(document.getElementById('progressIndicator'));
     progressIndicator.show();
 
-    // To Do: change this to call back end function that returns 
-    var userGoal = goal.getStoredGoal(); 
-    var image = getImageToDisplay("travel", currentImage); 
+    var userGoal = goal.getStoredGoal(); // To Do: change this to call back end function that returns 
+    var currentImageURL = getImageToDisplay("travel", imageNum); 
+    imageNum = (imageNum +1)%3;
+    var nextImageURL = getImageToDisplay("travel", imageNum); 
 
-    document.getElementById("saving-screen").style.background = "url(" + image +") no-repeat center center fixed";
+    PreloadImage(currentImageURL);
+    PreloadImage(nextImageURL);
+
+    document.getElementById("saving-screen").style.background = "url(" + currentImageURL +") no-repeat center center fixed";
     document.getElementById("saving-screen").style.backgroundSize = "auto 100%";
-    // document.getElementById("saving-screen-bottom").style.background = "url(" + image +") no-repeat center center fixed";
-    // document.getElementById("saving-screen-bottom").style.backgroundSize = "auto 100%";
 
     document.ontouchmove = function(event){
       event.preventDefault();
     }
 
+    // Reveal the clear image when the user holds down on the screen
     $scope.unblur = function() {
       document.getElementById("saving-screen").className="unblur";
-      //document.getElementById("saving-screen-bottom").className="unblur";
       $scope.message = ""; 
       progressIndicator.start();
       logger.log("unblur called"); 
     };
 
+    // Reblur the image and begin transition to next image when user releases hold on screen 
     $scope.reblur = function() {
       document.getElementById("saving-screen").className= "blur";
-      //document.getElementById("saving-screen-bottom").className= "blur";
       progressIndicator.stop();
 
       var userGoal = goal.getStoredGoal(); 
@@ -78,13 +89,15 @@ angular.module('spearmintWebApp')
       
     };
 
+    // Continuation of the reblur function 
     var continueReblur = function(userGoal) { 
-      logger.log("before save: " + userGoal); 
-
+      // Get and save user progress toward their goal
       var dollarAmount = progressIndicator.getAmount();
       userGoal.amountSaved += dollarAmount; 
       goal.save(userGoal); 
+      goalService.saveGoal(userGoal);
 
+      // Display messaging to indicate progress 
       if (userGoal.amountSaved > userGoal.targetAmount) {
         $scope.message = "Congratulations! You have reached your goal of saving " + userGoal.targetAmount + ". Now you can " + userGoal.name + "!"; 
       } else if (userGoal.amountSaved > 0) {
@@ -95,48 +108,27 @@ angular.module('spearmintWebApp')
 
       progressIndicator.reset(); 
 
-      goalService.saveGoal(userGoal);
+      // Wait 2 seconds for reblur animation to stop, then transition to next image 
+      setTimeout(function(){transitionToNextImage(userGoal)}, 2000);
 
-
-      //setTimeout(function(){transitionToNextImage(userGoal)}, 2000);
-
-      transitionToNextImage(userGoal);
-
-
-      logger.log("after save: " + userGoal); 
-
-      logger.log("reblur called"); 
     };
 
-    var resetImage = function(image) { 
-      document.getElementById("saving-screen").style.background = "url(" + image +") no-repeat center center fixed";
-      document.getElementById("saving-screen").style.backgroundSize = "auto 100%";
-      document.getElementById("saving-screen").className = "blur";
-    };
 
     var transitionToNextImage = function(userGoal) { 
       var userGoal = goal.getStoredGoal(); 
 
-      currentImage += 1; 
-      if (currentImage > 2) { 
-        currentImage = 0; 
-      }
-      logger.log(currentImage);
-
-      var image = getImageToDisplay("travel", currentImage); 
-      logger.log(image);
-
-      document.getElementById("saving-screen").style.background = "url(" + image +") no-repeat center center fixed";
+      document.getElementById("saving-screen").style.background = "url(" + nextImageURL +") no-repeat center center fixed";
       document.getElementById("saving-screen").style.backgroundSize = "auto 100%";
-      // document.getElementById("saving-screen").className = "transition blur";
 
-      // setTimeout(function() {resetImage(image)}, 3000); 
+      // Preload the next images to display 
+      imageNum = (imageNum +1)% 3; 
+      currentImageURL = nextImageURL; 
+      nextImageURL = getImageToDisplay("travel", imageNum); // To Do: make the image reflect user goal 
+      PreloadImage(nextImageURL);
 
     };
 
-
-
-  }]);
+}]);
 
 
 
