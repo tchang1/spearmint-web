1f23373ccb3ea5e9149349708a0981e37ca49d2e
Matt Ziegler <matthew_ziegler@intuit.com>
2014-06-25 16:13:15 -0700

    Fixed odd margin issues at the bottom of the page for FTU

--- app/scripts/controllers/welcome.js
+++ app/scripts/controllers/welcome.js
@@ -141,40 +141,4 @@ angular.module('spearmintWebApp')
 
     };
 
-
-//    progressIndicator.initWithCanvas(document.getElementById('progressIndicator'));
-//
-//    progressIndicator.show();
-//
-//    document.onmousedown=function(){
-//        progressIndicator.start();
-//    };
-//
-//    document.onmouseup=function(){
-//        progressIndicator.stop();
-//        var dollarAmount = progressIndicator.getAmount();
-//    };
-    // example of how to log in and retrieve the user's goal
-
-    // userService.login('test', 'test123').then(
-    // function(result) {
-    //     logger.log('Result: ' + result);
-    //     goalService.getGoal().then(
-    //         // success handler
-    //         function(result) {
-    //             logger.log(result);
-    //         },
-
-    //         // error handler
-    //         function(error) {
-    //             logger.error(error);
-    //         }
-    //     )
-    //     },
-
-    //     function(error) {
-    //         logger.error(error);
-    //     }
-    // );
-
   }]); 
