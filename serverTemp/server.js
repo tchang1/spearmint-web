8b3d3cbd01c146763e00be5017bcdb402897bcd5
Tony Chang <ampedlemon@gmail.com>
2014-06-26 11:35:48 -0700

    server config files

---
+++ serverTemp/server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var express = require('express'),
+    path = require('path'),
+    fs = require('fs'),
+    dummydata=require('./lib/config/dummydata');
+
+/**
+ * Main application file
+ */
+
+// Set default node environment to development
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var config = require('./lib/config/config');
+
+//dummydata();
+
+// Setup Express
+var app = express();
+require('./lib/config/express')(app);
+require('./lib/routes')(app);
+
+// Start server
+config.port=80;
+app.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
